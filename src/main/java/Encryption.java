1bd4c4291f2f84618d908b79b02802e887ac8eeb
Dries Fierens <dries.fierens@student.odisee.be>
2022-05-26 13:05:43 +0200

    Fixed weak algorithmes

--- src/main/java/Encryption.java
+++ src/main/java/Encryption.java
@@ -1,6 +1,7 @@
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -22,12 +23,12 @@ public class Encryption {
     }
 
     public static String encrypt(String dataToEncrypt, SecretKeySpec key) throws GeneralSecurityException, UnsupportedEncodingException {
-        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        Cipher pbeCipher = Cipher.getInstance("AES/GCM/NoPadding");
         pbeCipher.init(Cipher.ENCRYPT_MODE, key);
         AlgorithmParameters parameters = pbeCipher.getParameters();
-        IvParameterSpec ivParameterSpec = parameters.getParameterSpec(IvParameterSpec.class);
+        GCMParameterSpec gcmParameterSpec = parameters.getParameterSpec(GCMParameterSpec.class);
         byte[] cryptoText = pbeCipher.doFinal(dataToEncrypt.getBytes("UTF-8"));
-        byte[] iv = ivParameterSpec.getIV();
+        byte[] iv = gcmParameterSpec.getIV();
         return base64Encode(iv) + ":" + base64Encode(cryptoText);
     }
 
@@ -38,8 +39,8 @@ public class Encryption {
     public static String decrypt(String string, SecretKeySpec key) throws GeneralSecurityException, IOException {
         String iv = string.split(":")[0];
         String property = string.split(":")[1];
-        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        pbeCipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(base64Decode(iv)));
+        Cipher pbeCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        pbeCipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, base64Decode(iv)));
         return new String(pbeCipher.doFinal(base64Decode(property)), "UTF-8");
     }
 
